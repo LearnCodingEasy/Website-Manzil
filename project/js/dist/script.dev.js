46d632ec616efffabe168fbe43f2204e948a1dfe
Hossam Rashad <learncodingeasy@yahoo.com>
2023-02-20 18:02:58 +0200

    Framework Css

--- project/js/dist/script.dev.js
+++ project/js/dist/script.dev.js
@@ -52,3 +52,82 @@ $(".slider-active").nivoSlider({
   // afterLoad: function () {},
 
 });
+/*
+=====================================================
+Todo =============== DARK LIGHT THEME ===============
+=====================================================
+هرجع اشغلهاء 
+*/
+
+var themeButton = document.getElementById("change-theme-icon");
+var darkTheme = "dark-theme";
+var iconTheme = "fa-sun"; // Previously selected topic (if user selected)
+
+var selectedTheme = localStorage.getItem("selected-theme");
+var selectedIcon = localStorage.getItem("selected-icon"); // We obtain the current theme that the interface has by validating the dark-theme class
+
+var getCurrentTheme = function getCurrentTheme() {
+  return document.body.classList.contains(darkTheme) ? "dark" : "light";
+};
+
+var getCurrentIcon = function getCurrentIcon() {
+  return themeButton.classList.contains(iconTheme) ? "fa-moon" : "fa-sun";
+}; // We validate if the user previously chose a topic
+
+
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](darkTheme);
+  themeButton.classList[selectedIcon === "fa-moon" ? "add" : "remove"](iconTheme);
+} // Activate / deactivate the theme manually with the button
+
+
+themeButton.addEventListener("click", function () {
+  console.log("Yes"); // Add or remove the dark / icon theme
+
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme); // We save the theme and the current icon that the user chose
+
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
+/*
+=====================================================
+Todo =============== Owl Carousel ===================
+=====================================================
+*/
+
+$(document).ready(function () {
+  // owl
+  $(".teamMembers-slider .owl-carousel").owlCarousel({
+    loop: true,
+    margin: 30,
+    nav: true,
+    dots: false,
+    autoplay: false,
+    smartSpeed: 1000,
+    responsive: {
+      0: {
+        items: 2
+      },
+      600: {
+        items: 2
+      },
+      1000: {
+        items: 3
+      }
+    }
+  });
+});
+/*
+=====================================================
+Todo =============== mixitup ===================
+=====================================================
+*/
+
+var mixer = mixitup(".mixitup-items", {
+  animation: {
+    duration: 300,
+    effectsOut: "fade translateX(-100%)"
+  }
+});
--- project/js/script.js
+++ project/js/script.js
@@ -50,3 +50,85 @@ $(".slider-active").nivoSlider({
   // lastSlide: function () {},
   // afterLoad: function () {},
 });
+
+/*
+=====================================================
+Todo =============== DARK LIGHT THEME ===============
+=====================================================
+هرجع اشغلهاء 
+*/
+const themeButton = document.getElementById("change-theme-icon");
+const darkTheme = "dark-theme";
+const iconTheme = "fa-sun";
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? "fa-moon" : "fa-sun";
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === "fa-moon" ? "add" : "remove"](
+    iconTheme
+  );
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener("click", () => {
+  console.log(`Yes`);
+  // Add or remove the dark / icon theme
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  // We save the theme and the current icon that the user chose
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
+
+/*
+=====================================================
+Todo =============== Owl Carousel ===================
+=====================================================
+*/
+$(document).ready(function () {
+  // owl
+  $(".teamMembers-slider .owl-carousel").owlCarousel({
+    loop: true,
+    margin: 30,
+    nav: true,
+    dots: false,
+    autoplay: false,
+    smartSpeed: 1000,
+    responsive: {
+      0: {
+        items: 2,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
+    },
+  });
+});
+
+/*
+=====================================================
+Todo =============== mixitup ===================
+=====================================================
+*/
+var mixer = mixitup(".mixitup-items", {
+  animation: {
+    duration: 300,
+    effectsOut: "fade translateX(-100%)",
+  },
+});
